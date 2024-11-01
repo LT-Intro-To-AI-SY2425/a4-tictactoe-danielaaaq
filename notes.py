31af1e61aa208a614624e38eafa27bdd77077b37
danielaaaq <dmolina12@cps.edu>
2024-11-01 10:38:09 -0500

    added more

--- notes.py
+++ notes.py
@@ -2,3 +2,26 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog :
+    species = "canis familiaris"
+
+    def __init__(self, n = "no name", a = 0):
+        self.name = n
+        self.age = a
+        self.fetch_count = 0
+
+    def __str__(self):
+        s = f"{self.name} is {self.age} years old"
+        return s 
+    
+    def play_fetch(self, num_fetch):
+        self.fetch_count += num_fetch 
+
+
+logan = Dog("logan",8)
+becker = Dog ("becker",4)
+kepa = Dog ("kepa",4)
+
+print(logan.fetch_count)
+print(becker)
+print(kepa)
